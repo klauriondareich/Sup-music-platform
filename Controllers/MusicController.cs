aa01d4ffce667b1c2353777fc1bfb28d50b09c93
laurion <klauriondareich@gmail.com>
2022-02-12 21:58:37 +0100

    MusicController updated

--- Controllers/MusicController.cs
+++ Controllers/MusicController.cs
@@ -16,29 +16,30 @@ namespace MusicAudioPlayer.Controllers
         }
 
         // GET: Display a create music view
-        [Route("music/create")]
+        [Route("music/create/{playlistId}")]
         public ActionResult Create()
         {
             return View();
         }
 
         // POST: Save a music in Db
-        [Route("music/create")]
+        [Route("music/create/{playlistId}")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind("Id", "title", "author", "published", "file_path", "playId")] Music myMusic)
+        public ActionResult Create(int playlistId, [Bind("Id", "title", "author", "published", "file_path")] Music myMusic)
         {
             if (ModelState.IsValid)
             {
+                myMusic.playId = playlistId;
                 _context.Add(myMusic);
                 _context.SaveChanges();
-                //return RedirectToAction("Display", "Playlist");
+                return RedirectToAction("Display", "Playlist", new { Id = playlistId });
             }
             return View();
         }
 
         // GET: Display edit music view
-        [Route("music/edit/{Id}")]
+        [Route("music/edit/{playlistId}/{Id}")]
         public ActionResult Edit(int Id)
         {
             var myMusic = _context.Musics.FirstOrDefault(n => n.Id == Id)!;
@@ -46,18 +47,24 @@ namespace MusicAudioPlayer.Controllers
         }
 
         // UPDATE: Edit a music
-        [Route("music/edit/{Id}")]
+
+        [Route("music/edit/{playlistId}/{Id}")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int Id, [Bind("Id", "title", "author", "published", "file_path", "playId")] Music myMusic)
+        public ActionResult Edit(int Id, int playlistId, [Bind("Id", "title", "author", "published", "file_path")] Music myMusic)
         {
             if (Id != myMusic.Id) return NotFound();
 
             if (!ModelState.IsValid) return View(myMusic);
 
+            myMusic.playId = playlistId;
+
             _context.Update(myMusic);
             _context.SaveChanges();
-            return RedirectToAction("Display", "Playlist");
+
+            myMusic.playId = playlistId;
+
+            return RedirectToAction("Display", "Playlist", new {Id = playlistId});
         }
 
         // DELETE: Delete a music
@@ -68,9 +75,9 @@ namespace MusicAudioPlayer.Controllers
             {
                 _context.Musics.Remove(myMusic);
                 _context.SaveChanges();
-                return RedirectToAction("Display", "Playlist");
             }
-            return View(myMusic);
+            return RedirectToAction("Display", "Playlist");
+
         }
     }
 }
