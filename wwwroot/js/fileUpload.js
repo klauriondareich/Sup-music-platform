314f5530d06217b86b174a255dbbe56e33cca6f4
laurion <klauriondareich@gmail.com>
2022-02-12 22:01:29 +0100

    asp.net updates

---
+++ wwwroot/js/fileUpload.js
@@ -0,0 +1,21 @@
+﻿// Upload files
+
+function uploadFiles() {
+
+    var input = document.querySelector('input[type="file"]');
+
+    let file = input.files[0];
+    let fileName = file.name;
+    let newName = file.lastModified + file.name;
+    let newFile = new File([fileName], newName, { type: 'image/jpeg' })
+    console.log(newName);
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch("/music", {
+        method: "POST",
+        body: formData
+    })
+        .then(success => console.log("image uploaded"))
+        .catch(error => console.log(error))
+}
