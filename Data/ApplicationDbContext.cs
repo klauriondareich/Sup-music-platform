805829e2085fb37774e8c69be7cf0d6bc3f76b19
laurion <klauriondareich@gmail.com>
2022-02-13 16:22:40 +0100

    login info displayed in navbar when user connected

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MusicAudioPlayer.Models;
 
@@ -6,6 +7,15 @@ namespace MusicAudioPlayer.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        /*protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityUser>()
+                .HasMany<Myplaylist>()
+                .WithMany(p => p.User)
+                .HasForeignKey(p => p.userId);
+        }*/
+
         public DbSet<Myplaylist> Myplaylist { get; set; }
 
         public DbSet<Music> Musics { get; set; }
--- Models/Myplaylist.cs
+++ Models/Myplaylist.cs
@@ -1,9 +1,14 @@
-﻿namespace MusicAudioPlayer.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MusicAudioPlayer.Models
 {
     public class Myplaylist
     {
          public int Id {get; set;}
         public string? title {get; set;}
         public string? imagePath {get; set;}
+
+       /* public virtual IdentityUser? User {get; set;}
+        public string? userId {get; set; }*/
     }
 }
